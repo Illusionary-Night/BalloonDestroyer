fdee87917058fabece5b0d0e1e2726e286b01061
32982920 <shengasheng19@gmail.com>
2025-11-15 10:59:06 +0800

    Update Boat.cs

--- BalloonDestroyer/Assets/Scripts/Objects/Boat.cs
+++ BalloonDestroyer/Assets/Scripts/Objects/Boat.cs
@@ -1,16 +1,45 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
-public class Boat : MonoBehaviour
+public class Balloon : MonoBehaviour, IBlowable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] float speed = 3f;
+    [SerializeField] LayerMask obstacleMask; // 指定 Rocks, Fans, Boats 所在 Layer
+    bool isMoving = false;
+
+    public void StartMove(direction finalWinddirection)
     {
-        
+        if (isMoving) return;
+        Vector2 dir = MovementHelper.directionToVector(finalWinddirection);
+        if (dir == Vector2.zero) return;
+        StartCoroutine(MoveAlongWind(dir));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator MoveAlongWind(Vector2 dir)
     {
-        
+        isMoving = true;
+        while (true)
+        {
+            Sprite WhatIsAtForward = tilemap.WorldToCell(tilemap.GetSprite(transform.position + new Vector3(dir.x, dir.y, 0)));
+            if (WhatIsAtForward != Water_0) break;
+            // 檢查前方一格是否被阻擋
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 1f, obstacleMask);
+            if (hit.collider != null) break;
+
+            Vector3 target = transform.position + (Vector3)dir;
+            while ((transform.position - target).sqrMagnitude > 0.0001f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+                yield return null;
+            }
+            transform.position = target;
+
+            yield return null;
+        }
+        isMoving = false;
     }
+
+    public bool IsMoving() => isMoving;
+    public int GetPriority() => 1;
 }
