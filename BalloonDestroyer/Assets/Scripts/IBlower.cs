4f15822155586bc9133bfde00c9e593cc6f67ede
Illusionary-Night <0s5h1a2s@gmail.com>
2025-11-15 11:16:30 +0800

    Refactor IBlower interface methods
    Replaced StartMove, IsMoving, and GetPriority methods with SetWindStrength, GetWindStrength, and PositionIsInfluenced in the IBlower interface. This refactor updates the interface to better reflect wind strength handling and position influence logic.

--- BalloonDestroyer/Assets/Scripts/IBlower.cs
+++ BalloonDestroyer/Assets/Scripts/IBlower.cs
@@ -3,18 +3,10 @@
 
 interface IBlower
 {
-    /// <summary>
-    /// GameManager 呼叫這個來命令物件根據風向開始移動
-    /// </summary>
-    void StartMove(direction finalWinddirection);
+    void SetWindStrength(int strength);
+    int GetWindStrength();
+    bool PositionIsInfluenced(Vector2 pos)
+    {
 
-    /// <summary>
-    /// GameManager 呼叫這個來檢查物件是否還在移動
-    /// </summary>
-    bool IsMoving();
-
-    /// <summary>
-    /// GameManager 呼叫這個來排序 (帆船回傳 1, 氣球回傳 0)
-    /// </summary>
-    int GetPriority();
+    }
 }
