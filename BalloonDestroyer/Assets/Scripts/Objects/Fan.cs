0ce6b1ec5225dc6a5c751885962c67365ee58fdf
Illusionary-Night <0s5h1a2s@gmail.com>
2025-11-15 09:18:00 +0800

    Update scene and add commented Fan wind logic
    Updated SampleScene.unity to a newer Unity serialization format and removed the Global Light 2D object. Added commented-out code in Fan.cs for a potential Iblow interface implementation, including wind direction logic for the fan.

--- BalloonDestroyer/Assets/Scripts/Objects/Fan.cs
+++ BalloonDestroyer/Assets/Scripts/Objects/Fan.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Fan : MonoBehaviour
@@ -14,3 +15,38 @@ public class Fan : MonoBehaviour
         
     }
 }
+//using UnityEngine;
+
+//public class fan : MonoBehaviour, Iblow
+//{
+//    Direction direction;
+//    Vector2Int position;
+//    public int GetPriority()
+//    {
+//        return 2;
+//    }
+//    public void Blow(Direction[,] windMap)
+//    {
+//        if (windMap == null) return;
+//        if (direction == Direction.UP)
+//        {
+//            for (int y = position.y; y < windMap.GetLength(1); y++) windMap[position.x, y] = Direction.UP;
+//        }
+//        else if (direction == Direction.DOWN)
+//        {
+//            for (int y = position.y; y >= 0; y--) windMap[position.x, y] = Direction.DOWN;
+//        }
+//        else if (direction == Direction.RIGHT)
+//        {
+//            for (int x = position.x; x < windMap.GetLength(0); x++) windMap[x, position.y] = Direction.RIGHT;
+//        }
+//        else if (direction == Direction.LEFT)
+//        {
+//            for (int x = position.x; x >= windMap.GetLength(0); x--) windMap[x, position.y] = Direction.LEFT;
+//        }
+//        else
+//        {
+//            Debug.Log("fan direction error");
+//        }
+//    }
+//}
